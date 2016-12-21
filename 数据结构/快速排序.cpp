8054bcb9087f75a601e589d880ee73cae29dd83c
Paddle_Pish <Paddle_Pish@SHD5-18>
2016-12-21 21:25:26 +0800

    jifang

--- 数据结构/快速排序.cpp
+++ 数据结构/快速排序.cpp
@@ -11,14 +11,21 @@ void q_sort(int lo, int hi)
 {
 	if(lo>=hi) return;//递归基
 	int low=lo, high=hi;
-	//int key=a[lo];
+
+	//三者取中
+	int kk=(low+high)/2;
+	if(a[kk]>a[low]&&a[kk]>a[high]) kk=(a[lo]>=a[high] ? lo : high);
+	else if(a[kk]<a[low]&&a[kk]<a[high]) kk=(a[lo]<=a[high] ? lo : high);
+	int key=a[kk];
+
 	while(low<high)
-	{//注意 
-		while(low<high&&a[low]<=a[high]) --high;
-		/*if(low!=high)*/ swap(a[low],a[high]);
-		while(a[low]<=a[high]&&low<high) ++low;
-		/*if(low!=high)*/ swap(a[low], a[high]);
+	{
+		while(low<high&&key<=a[high]) --high;
+		/*if(low!=high)*/ a[low]=a[high];
+		while(low<high&&a[low]<=key) ++low;
+		/*if(low!=high)*/ a[high]=a[low];
 	}
+	a[low]=key;
 	q_sort(lo, low-1);
 	q_sort(high+1, hi);
 }
---
+++ 数据结构/选择排序.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<algorithm>
+#include<cstring>
+#include<ctime>
+
+using namespace std;
+void SelectSort(int a[],int lo,int hi)
+{
+	int Ccount=0,Mcount=0;
+	//比较次数和初始状态无关，永远为n*(n-1)/2
+	for(int i=hi;i>=lo;i--)
+	{
+		int max_num=lo;
+		for(int j=lo+1;j<=i;j++)
+		{
+			if(++Ccount&&a[j]>a[max_num]) max_num=j;
+		}
+		if(i!=max_num)
+		{
+			swap(a[i],a[max_num]);
+			Mcount+=3;
+		}
+	}
+	cout<<"Ccount:"<<Ccount<<endl<<"Mcount:"<<Mcount<<endl;
+}
+
+int main()
+{
+	int a[10+1];
+	for(int i=1;i<=10;i++)
+	{
+		//scanf("%d",&a[i]);
+		a[i]=i;
+	}
+	SelectSort(a,1,10);
+	for(int i=1;i<=10;i++)
+	{
+		printf("%d%c",a[i],i%10==0 ? '\n' : ' ');
+	}
+	system("pause");
+	return 0;
+}
